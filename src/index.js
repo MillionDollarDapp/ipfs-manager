bef524aac60afd8a74c09890b931b1ce7586a1a1
haggins <patrick@mindf.org>
2018-09-03 19:57:28 +0200

    less stressing of the websocket endpoint

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const app = {
 
   start () {
     this.initWeb3()
-    this.mdapp = new this.web3.eth.Contract(MDAPPArtifact.abi, MDAPPArtifact.networks[config.web3.network].address)
     this.maintainConnection()
     this.findExpiredFiles()
   },
 
   initWeb3 () {
+    console.log(`Establishing connection to ${web3config.provider}`)
     this.provider = new Web3.providers.WebsocketProvider(web3config.provider)
     this.provider.on('connect', () => {
       this.isConnected = true
@@ -34,46 +34,49 @@ const app = {
       console.log('connection lost, reconnect...')
       setTimeout(() => {
         this.initWeb3()
-      }, 500)
+      }, 5000)
     })
 
     this.web3 = new Web3(this.provider)
+    this.mdapp = new this.web3.eth.Contract(MDAPPArtifact.abi, MDAPPArtifact.networks[config.web3.network].address)
   },
 
   async watch () {
-    try {
-      let startBlock = await utils.getVariable('lastEventBlock')
-      startBlock = startBlock ? parseInt(startBlock) + 1 : web3config.dappGenesis
-      console.log('begin watching at block:', startBlock)
+    if (this.isConnected) {
+      try {
+        let startBlock = await utils.getVariable('lastEventBlock')
+        startBlock = startBlock ? parseInt(startBlock) + 1 : web3config.dappGenesis
+        console.log('begin watching at block:', startBlock)
 
-      this.mdapp.events.EditAd({fromBlock: startBlock, toBlock: 'latest'})
-        .on('data', async (event) => {
-          let r = event.returnValues
-          try {
-            let hash = utils.multihash2hash(r.hashFunction, r.digest)
-            await utils.addToIPFS(hash)
-            utils.removeHashFromDynamoDb(hash)
+        this.mdapp.events.EditAd({fromBlock: startBlock, toBlock: 'latest'})
+          .on('data', async (event) => {
+            let r = event.returnValues
+            try {
+              let hash = utils.multihash2hash(r.hashFunction, r.digest)
+              await utils.addToIPFS(hash)
+              utils.removeHashFromDynamoDb(hash)
 
-            // If this script was down while multiple events emitted, the execution order is not guaranteed.
-            if (event.blockNumber > startBlock) {
-              startBlock = event.blockNumber
-              utils.setVariable('lastEventBlock', event.blockNumber)
+              // If this script was down while multiple events emitted, the execution order is not guaranteed.
+              if (event.blockNumber > startBlock) {
+                startBlock = event.blockNumber
+                utils.setVariable('lastEventBlock', event.blockNumber)
+              }
+
+              utils.removeFromUploadDir(hash)
+              console.log(`processed file ${hash} at block ${event.blockNumber}`)
+            } catch (e) {
+              console.error(e)
             }
-
-            utils.removeFromUploadDir(hash)
-            console.log(`processed file ${hash} at block ${event.blockNumber}`)
-          } catch (e) {
-            console.error(e)
-          }
-        })
-        .on('changed', event => {
-          console.log('event removed from blockchain:', event)
-        })
-        .on('error', error => {
-          console.error(error)
-        })
-    } catch (e) {
-      console.error(e)
+          })
+          .on('changed', event => {
+            console.log('event removed from blockchain:', event)
+          })
+          .on('error', error => {
+            console.error(error)
+          })
+      } catch (e) {
+        console.error(e)
+      }
     }
   },
 
@@ -81,62 +84,64 @@ const app = {
    * Send a packet to the server to maintain an idle connection.
    */
   async maintainConnection () {
-    try {
-      await this.web3.eth.net.isListening()
-    } catch (e) {}
-    setTimeout(() => { this.maintainConnection() }, 10000)
+    if (this.isConnected) {
+      try {
+        await this.web3.eth.net.isListening()
+      } catch (e) {}
+    }
+    setTimeout(() => { this.maintainConnection() }, 20000)
   },
 
   async findExpiredFiles () {
-    try {
-      let expired = await utils.getExpiredFiles(config.expireAfterSeconds)
-      if (expired.length) {
-        let mhs = new Map()
-        let digests =[]
-        for (let i = 0; i < expired.length; i++) {
-          let mh = utils.hash2multihash(expired[i])
-          mhs.set(mh.digest, mh)
-          digests.push(mh.digest)
-        }
-
-        // Double check if those hashes really really aren't mined into blockchain. This can happen if we miss an
-        // EditAd event above (server glitch or whatever).
-        // Look for EditAd events with the expired multihashes.
-        this.mdapp.getPastEvents('EditAd', {
-          filter: {digest: digests},
-          fromBlock: web3config.dappGenesis,
-          toBlock: 'latest'
-        })
-        .then(async (events) => {
-          // We found events with equal digest. Do hashFunction and size also fit?
-          try {
-            for (let i = 0; i < events.length; i++) {
-              let r = events[i].returnValues
-              let mh = mhs.get(r.digest)
-
-              let hash = utils.multihash2hash(mh.hashFunction, mh.digest)
-              if (r.hashFunction === mh.hashFunction && parseInt(r.size) === mh.size) {
-                // Phew! This file must not be deleted.
-                console.log('do not delete:', hash)
-                await utils.addToIPFS(hash)
-                utils.removeHashFromDynamoDb(hash)
-                utils.removeFromUploadDir(hash)
-              } else {
-                console.log('delete:', hash)
-                utils.removeHashFromDynamoDb(hash)
-                utils.removeFromS3(hash)
-                utils.removeFromUploadDir(hash)
-              }
-            }
-          } catch (e) {
-            console.error(e)
+    if (this.isConnected) {
+      try {
+        let expired = await utils.getExpiredFiles(config.expireAfterSeconds)
+        if (expired.length) {
+          let mhs = new Map()
+          let digests =[]
+          for (let i = 0; i < expired.length; i++) {
+            let mh = utils.hash2multihash(expired[i])
+            mhs.set(mh.digest, mh)
+            digests.push(mh.digest)
           }
-        }).catch (e => {
-          console.error(e)
-        })
+
+          // Double check if those hashes really really aren't mined into blockchain. This can happen if we miss an
+          // EditAd event above (server glitch or whatever).
+          // Look for EditAd events with the expired multihashes.
+          this.mdapp.getPastEvents('EditAd', {
+            filter: {digest: digests},
+            fromBlock: web3config.dappGenesis,
+            toBlock: 'latest'
+          })
+          .then(async (events) => {
+            // We found events with equal digest. Do hashFunction and size also fit?
+            try {
+              for (let i = 0; i < events.length; i++) {
+                let r = events[i].returnValues
+                let mh = mhs.get(r.digest)
+
+                let hash = utils.multihash2hash(mh.hashFunction, mh.digest)
+                if (r.hashFunction === mh.hashFunction && parseInt(r.size) === mh.size) {
+                  // Phew! This file must not be deleted.
+                  console.log('do not delete:', hash)
+                  await utils.addToIPFS(hash)
+                  utils.removeHashFromDynamoDb(hash)
+                  utils.removeFromUploadDir(hash)
+                } else {
+                  console.log('delete:', hash)
+                  utils.removeHashFromDynamoDb(hash)
+                  utils.removeFromS3(hash)
+                  utils.removeFromUploadDir(hash)
+                }
+              }
+            } catch (e) {
+              console.error(e)
+            }
+          })
+        }
+      } catch (e) {
+        console.error(e)
       }
-    } catch (e) {
-      console.error(e)
     }
     setTimeout(() => { this.findExpiredFiles() }, 5000)
   }
